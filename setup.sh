88ba246b74a0ae77e7f9deb45dd3bf07af49eb93
Wassim KABALAN <wastondev@gmail.com>
2024-06-13 12:04:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -138,6 +138,6 @@ backup
 echo "==================================="
 echo "Installing NvChad and custom parameters..."
 echo "==================================="
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
 git clone https://github.com/ASKabalan/my-nvim-config ~/.config/nvim/lua/custom --depth 1
 nvim
